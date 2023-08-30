96a46a22d37fa5daa0271011e2f3eabe9369b1cc
simonebesutti23 <118028228+simonebesutti23@users.noreply.github.com>
2023-08-30 22:26:25 +0200

    main

---
+++ javascript/main.js
@@ -0,0 +1,24 @@
+/*document.addEventListener('scroll' , () => {
+    let navbar = document.querySelector('.navbar');
+
+    if (window.scrollY > 0){
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+})*/
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));

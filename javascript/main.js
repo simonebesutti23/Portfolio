85fca18ff4ffe3be363c8a3d794080dc4707602c
simonebesutti23 <118028228+simonebesutti23@users.noreply.github.com>
2023-09-27 00:58:56 +0200

    finished
    finished

--- javascript/main.js
+++ javascript/main.js
@@ -23,3 +23,39 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
+
+const contactForm = document.querySelector("#form");
+const submitBtn = document.querySelector("#submit-btn");
+const nameInput = document.querySelector("#fullName");
+const emailInput = document.querySelector("#emailId");
+const messageInput = document.querySelector("#message");
+
+
+const publicKey = "ZOsENfQL-H8edUhqt";
+const serviceId = "service_6lch37a";
+const templateId = "template_upu0jsq"
+
+emailjs.init(publicKey);
+
+contactForm.addEventListener("submit" , e => {
+    e.preventDefault();
+    submitBtn.innerText = "Just a moment...."
+
+    const inputFields = {
+        name: nameInput.value,
+        email: emailInput.value,
+        message: messageInput.value
+    }
+
+
+    emailjs.send(serviceId, templateId, inputFields)
+    .then(() => {
+        submitBtn.innerText = "Message Sent Successfully";
+        nameInput.value = "";
+        emailInput.value = "";
+        messageInput.value = "";
+    }, (error) => {
+        console.log(error);
+        submitBtn.innerText = "Something went wrong"
+    });
+});
